1a04c165512b2b23c89a93cbf01a016018d2ec94
ChrisRawstone <dr.rawstone@outlook.com>
2024-05-13 13:39:42 +1000

    init

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ var x_prev, y_prev;
 
 var gridSize = 160; // this determines the map size
 var line_segment_size = 20; // this will make longer roads if it is higher
-var iterations_of_Lsystem = 5; // the higher this value is the more roads will be generated
+var iterations_of_Lsystem = 10; // the higher this value is the more roads will be generated
 var weight_bias = 100; // the higher this value is the symmetric the roads will be. This can be anywhere from 0 to 10000
 var bias_half_life = 0.5; // the lower this value is the less symetric the roads will be from the middle. This can be anywhere from 0 to 1
 
@@ -57,6 +57,7 @@ var probability_of_tree = 0.05; // this is the probability of a tree being place
 
 var scale_of_tree = 0.2; // this is the scale of the tree
 
+
 // Starting positions of the first road - if you are unsure of what to put here, just leave it as is
 var x = Math.floor(gridSize * 1 / 4);
 var y = Math.floor(gridSize / 2);
@@ -64,46 +65,53 @@ var y = Math.floor(gridSize / 2);
 
 
 
+
 grid = init_grid(gridSize);
-[grid, x, x_prev, y, y_prev] = initialize_starting_road(grid, gridSize, x, y);
-populateGridWithRoadsRecursively(grid, x, y, x_prev, y_prev, iterations_of_Lsystem, gridSize, line_segment_size, weight_bias, bias_half_life);
+// Initialize road starting point based on mouse drag
+[grid, x, x_prev, y, y_prev] = initialize_starting_road(grid, gridSize, startX, startY);
+
 
 // print array nicely
 // print my grid array nicely
 
 
+controls.update();
 
-// this is placing buildings, trees, and supermarkets on the grid
-placeSupermarkets(grid,gridSize, probability_of_supermarket);
-placeBuildings(grid,gridSize,probability_of_building, 5);
-placeTrees(grid,gridSize,probability_of_tree);
+// Create and add cells to the scene based on the grid
+scene = createCanvas(grid, gridSize, scene);
 
-// for (let i = 0; i < gridSize; i++) {
-//     console.log(grid[i].join(" "));
-// }
+// Function to update grid and mesh color on mouse interaction
+function handleCanvasClick(event) {
+    const mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
-// this is visualizing the grid
-createCanvas(grid,gridSize,scene);
+    // Raycasting to find intersected objects
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
 
+    if (intersects.length > 0) {
+        const intersect = intersects[0];
+        const mesh = intersect.object;
+        const gridX = Math.floor(intersect.point.z + gridSize / 2);
+        const gridY = Math.floor(intersect.point.x + gridSize / 2);
 
+        if (grid[gridX][gridY] === 0) { // Check if the cell is empty before drawing
+            grid[gridX][gridY] = 1;
+            mesh.material.color.set(1, 1, 1); // Set to white
+        }
+    }
+}
 
-// These are placing Three.js objects in the scene
-// addTrees(grid, gridSize, scene);
-preloadTrees(scene, () => {
-    addTrees(grid, gridSize, scene,scale_of_tree);
-});
+// Add event listener to the renderer
+renderer.domElement.addEventListener('mousedown', handleCanvasClick, false);
 
-addSupermarkets(grid, gridSize, scene);
-addBuildings(grid, gridSize, scene);
-
-
-
-
-
-// Render loop
+// Animation loop to render the scene
 function animate() {
     requestAnimationFrame(animate);
-    renderer.render(scene, camera);
     controls.update();
+    renderer.render(scene, camera);
 }
+
 animate();
